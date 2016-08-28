3eb1306cb6adae83a7c4b6d70d09cc227b1c1c12
Johan Larsson <johanlarsson@outlook.com>
2016-08-28 21:08:41 +0200

    Tests for the main function qualpal().

---
+++ tests/testthat/test_qualpal.R
@@ -0,0 +1,17 @@
+library(qualpalr)
+context("qualpal() tests")
+
+test_that("qualpal() returns the proper list object", {
+  fit <- qualpal(n = 2)
+  expect_is(fit, "list")
+  expect_is(fit, "qualpal")
+  expect_equal(length(fit), 6)
+  expect_equal(length(fit$hex), 2)
+  })
+
+test_that("erroneous input to qualpal() returns errors", {
+  expect_error(qualpal(n = 0))
+  expect_error(qualpal("colorblind"))
+  expect_error(qualpal(2, "clorblind"))
+  expect_error(qualpal(2, list(h = 2, s = c(0, 1), l = c(0, 1))))
+})
