1903b04185a04931adb669f41d12336694d4506d
Johan Larsson <johanlarsson@outlook.com>
2016-08-28 14:51:50 +0200

    Add dependencies to functions.

--- R/wrappers.R
+++ R/wrappers.R
@@ -1,12 +1,12 @@
 # Optimization wrapper for dE CIEDE2000 -----------------------------------
 
 opt_dE <- function(par, n) {
-  Lab <- convertColor(t(apply(matrix(par, ncol = 3), 1, hsl_rgb)),
-                      from = "sRGB", to = "Lab")
+  Lab <- grDevices::convertColor(t(apply(matrix(par, ncol = 3), 1, hsl_rgb)),
+                                 from = "sRGB", to = "Lab")
 
-  L <- combn(Lab[, 1], 2)
-  a <- combn(Lab[, 2], 2)
-  b <- combn(Lab[, 3], 2)
+  L <- utils::combn(Lab[, 1], 2)
+  a <- utils::combn(Lab[, 2], 2)
+  b <- utils::combn(Lab[, 3], 2)
 
   min(CIEDE2000(L1 = L[1, ], a1 = a[1, ], b1 = b[1, ],
                 L2 = L[2, ], a2 = a[2, ], b2 = b[2, ]))
@@ -15,9 +15,9 @@ opt_dE <- function(par, n) {
 # Compute distance matrix of delta E CIEDE 2000 ---------------------------
 
 measure_CIEDE2000 <- function(lab_data, n) {
-  L <- combn(lab_data[, 1], 2)
-  a <- combn(lab_data[, 2], 2)
-  b <- combn(lab_data[, 3], 2)
+  L <- utils::combn(lab_data[, 1], 2)
+  a <- utils::combn(lab_data[, 2], 2)
+  b <- utils::combn(lab_data[, 3], 2)
 
   col_diff <- CIEDE2000(L1 = L[1, ], a1 = a[1, ], b1 = b[1,],
                         L2 = L[2, ], a2 = a[2, ], b2 = b[2, ])
@@ -31,5 +31,5 @@ measure_CIEDE2000 <- function(lab_data, n) {
   diff_mat[lower.tri(diff_mat)] <- col_diff
   diff_mat[upper.tri(diff_mat)] <- t(diff_mat)[upper.tri(diff_mat)]
 
-  as.dist(diff_mat)
+  stats::as.dist(diff_mat)
 }
