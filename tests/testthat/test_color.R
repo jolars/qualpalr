0bc9da77710c990e8ef0a14ecd1cc498d839d6a5
Johan Larsson <johanlarsson@outlook.com>
2016-09-05 09:37:20 +0200

    Add new tests for color blindness simulations.

--- tests/testthat/test_color.R
+++ tests/testthat/test_color.R
@@ -1,5 +1,5 @@
 library(qualpalr)
-context("Color formulas")
+context("color formula tests")
 
 test_that("color formula conversions work properly", {
   # Data retrieved from https://en.wikipedia.org/wiki/HSL_and_HSV
@@ -47,4 +47,40 @@ test_that("formulas perform as expected on worked example from Cui 2002", {
     XYZ_DIN99d(xyz1, Xr = 0.9481, Yr = 1, Zr = 1.0733),
     XYZ_DIN99d(xyz2, Xr = 0.9481, Yr = 1, Zr = 1.0733)
   ))), 3.1210819361106368)
+})
+
+test_that("color blindness simulation performs on test data", {
+  # Test data from Vienot et al 1999
+  test_rgb <-   rbind(c(255, 255, 255),
+                      c(  0, 255, 255),
+                      c(255,   0, 255),
+                      c(255, 255,   0),
+                      c(  0,   0, 255),
+                      c(255,   0,   0),
+                      c(102,  51, 153)) / 255
+
+  protan_key <- rbind(c(255, 255, 255),
+                      c(241, 241, 254),
+                      c( 96,  96, 255),
+                      c(255, 255,  21),
+                      c( 21,  21, 255),
+                      c( 96,  96,  28),
+                      c( 62,  62, 153)) / 255
+
+  deutan_key <- rbind(c(253, 253, 253),
+                      c(217, 217, 255),
+                      c(148, 148, 250),
+                      c(253, 253,  44),
+                      c( 44,  44, 253),
+                      c(148, 148,   0),
+                      c( 80,  80, 154)) / 255
+
+  test_rgb <- test_rgb ^ 2.2
+  protan <- RGB_LMS(test_rgb * 0.992052 + 0.003974)
+  deutan <- RGB_LMS(test_rgb * 0.957237 + 0.0213814)
+  rgb_protan <- LMS_RGB(LMS_protan(protan)) ^ (1 / 2.2)
+  rgb_deutan <- LMS_RGB(LMS_deutan(deutan)) ^ (1 / 2.2)
+
+  expect_false(any(abs(protan_key - rgb_protan) > 0.01))
+  expect_false(any(abs(deutan_key - rgb_deutan) > 0.01))
 })
