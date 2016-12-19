2d8a4e6118ef2f558e67e2ae750be357e16bdab0
Johan Larsson <johanlarsson@outlook.com>
2016-12-19 21:48:16 +0100

    Drop test for old cvd conversions.

--- tests/testthat/test_color.R
+++ tests/testthat/test_color.R
@@ -18,10 +18,6 @@ test_that("color conversions work back and forth", {
                     test_rgb <
                     .Machine$double.eps ^ 0.5))
 
-  test_lms <- RGB_LMS(matrix(runif(9), ncol = 3))
-  expect_true(all(XYZ_LMS(LMS_XYZ(test_lms)) -
-                    test_lms <
-                    .Machine$double.eps ^ 0.5))
   test_xyz <- matrix(runif(9), ncol = 3)
   expect_equal(sum(test_xyz), sum(Lab_XYZ(XYZ_Lab(test_xyz))))
 
@@ -49,38 +45,3 @@ test_that("formulas perform as expected on worked example from Cui 2002", {
   ))), 3.1210819361106368)
 })
 
-test_that("color blindness simulation performs on test data", {
-  # Test data from Vienot et al 1999
-  test_rgb <-   rbind(c(255, 255, 255),
-                      c(  0, 255, 255),
-                      c(255,   0, 255),
-                      c(255, 255,   0),
-                      c(  0,   0, 255),
-                      c(255,   0,   0),
-                      c(102,  51, 153)) / 255
-
-  protan_key <- rbind(c(255, 255, 255),
-                      c(241, 241, 254),
-                      c( 96,  96, 255),
-                      c(255, 255,  21),
-                      c( 21,  21, 255),
-                      c( 96,  96,  28),
-                      c( 62,  62, 153)) / 255
-
-  deutan_key <- rbind(c(253, 253, 253),
-                      c(217, 217, 255),
-                      c(148, 148, 250),
-                      c(253, 253,  44),
-                      c( 44,  44, 253),
-                      c(148, 148,   0),
-                      c( 80,  80, 154)) / 255
-
-  test_rgb <- test_rgb ^ 2.2
-  protan <- RGB_LMS(test_rgb * 0.992052 + 0.003974)
-  deutan <- RGB_LMS(test_rgb * 0.957237 + 0.0213814)
-  rgb_protan <- LMS_RGB(LMS_protan(protan)) ^ (1 / 2.2)
-  rgb_deutan <- LMS_RGB(LMS_deutan(deutan)) ^ (1 / 2.2)
-
-  expect_false(any(abs(protan_key - rgb_protan) > 0.01))
-  expect_false(any(abs(deutan_key - rgb_deutan) > 0.01))
-})
