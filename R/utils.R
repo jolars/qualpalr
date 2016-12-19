03b66278af1388e5edd6add327084478c61b0397
Johan Larsson <johanlarsson@outlook.com>
2016-12-19 21:47:45 +0100

    Drop old farthest points optimizer.

--- R/utils.R
+++ R/utils.R
@@ -1,25 +1,6 @@
+# Test approximate equality
 
-# Find the farthest n points ----------------------------------------------
-
-farthest_points <- function (data, n) {
-  # DIN99d distances with power tranformations from Huang 2014
-  dm <- edist(data)
-
-  r <- sample.int(nrow(dm), n)
-
-  dimnames(dm) <- list(
-    seq.int(1, nrow(dm)),
-    seq.int(1, ncol(dm))
-  )
-
-  repeat {
-    r_old <- r
-    for (i in 1:n) {
-      mm <- dm[r[-i], -r[-i], drop = FALSE]
-      k <- which.max(mm[(1:ncol(mm) - 1) * nrow(mm) + max.col(t(-mm))])
-      r[i] <- as.numeric(dimnames(mm)[[2]][k])
-    }
-    if (identical(r_old, r)) return(r)
-  }
+almost_equal <- function(x, y, tol = .Machine$double.eps ^ 0.5) {
+  ifelse(abs(x - y) < tol, TRUE, FALSE)
 }
 
