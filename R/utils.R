298c0e36f54dc19128e58f9974abd6cb6b560224
Johan Larsson <johanlarsson@outlook.com>
2016-12-22 17:46:18 +0100

    Drop almost_equal function.

--- R/utils.R
+++ R/utils.R
@@ -1,9 +1,3 @@
-# Test approximate equality -----------------------------------------------
-
-almost_equal <- function(x, y, tol = .Machine$double.eps ^ 0.5) {
-  ifelse(abs(x - y) < tol, TRUE, FALSE)
-}
-
 # Scale to range ----------------------------------------------------------
 
 scale_runif <- function(x, new_min, new_max) {
