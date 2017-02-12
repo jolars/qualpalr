f6c08010db49935e228eba2daa2b00a2a93a0da4
Johan Larsson <johanlarsson@outlook.com>
2017-02-12 14:01:18 +0100

    Allow setting custom axis labels.

--- R/plotting.R
+++ R/plotting.R
@@ -1,31 +1,32 @@
 #' Multidimensional scaling map of qualitative color palette
 #'
-#' Uses the colors in a \code{qualpalr} object to compute and plot a
+#' Uses the colors in a \link{qualpal} object to compute and plot a
 #' multidimensional scaling (MDS) map using \code{\link[stats]{cmdscale}} on the
 #' Delta E DIN99d distance matrix.
 #'
-#' @param x A list object of class \code{"qualpal"} generated from
+#' @param x An object of class \code{"qualpal"} generated from
 #'   \code{\link{qualpal}}.
 #' @param ... Arguments to pass on to \code{\link[graphics]{plot}}.
 #' @seealso \code{\link{qualpal}}, \code{\link{pairs.qualpal}},
 #'   \code{\link[graphics]{plot}}
 #'
 #' @examples
-#' col_pal <- qualpal(n = 3)
+#' col_pal <- qualpal(3)
 #' plot(col_pal)
 #' @export
 
 plot.qualpal <- function(x, ...) {
   args <- list(
-    x = stats::cmdscale(x$de_DIN99d, k = ifelse(length(x$hex) == 2, 1, 2)),
-    col = x$hex,
-    ylab = "dE DIN99d",
-    xlab = "dE DIN99d",
+    x = stats::cmdscale(x[["de_DIN99d"]],
+                        k = ifelse(length(x[["hex"]]) == 2, 1, 2)),
+    col = x[["hex"]],
     ...
   )
 
   if (is.null(args[["cex"]])) args[["cex"]] <- 3
   if (is.null(args[["pch"]])) args[["pch"]] <- 19
+  if (is.null(args[["xlab"]])) args[["xlab"]] <- "dE DIN99d"
+  if (is.null(args[["ylab"]])) args[["ylab"]] <- "dE DIN99d"
 
   do.call(graphics::plot, args)
 }
@@ -54,10 +55,11 @@ plot.qualpal <- function(x, ...) {
 pairs.qualpal <- function(x, colorspace = c("DIN99d", "HSL"), ...) {
   args <- list(
     x = switch(match.arg(colorspace), DIN99d = x[["DIN99d"]], HSL = x[["HSL"]]),
-    col = x$hex,
+    col = x[["hex"]],
     ...
   )
   if (is.null(args[["cex"]])) args[["cex"]] <- 3
   if (is.null(args[["pch"]])) args[["pch"]] <- 19
+
   do.call(graphics::pairs, args)
 }
