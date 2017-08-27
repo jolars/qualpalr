6008eb446c0242b71895014cef102476c53e8fd3
Johan Larsson <johanlarsson@outlook.com>
2017-08-27 15:12:56 +0200

    Change to evalCpp

--- R/qualpalr.R
+++ R/qualpalr.R
@@ -1,7 +1,5 @@
-#' @keywords internal
-"_PACKAGE"
-
-#' @importFrom Rcpp sourceCpp
+#' @importFrom Rcpp evalCpp
 #' @importFrom RcppParallel RcppParallelLibs
 #' @useDynLib qualpalr, .registration = TRUE
-NULL
+#' @keywords internal
+"_PACKAGE"
