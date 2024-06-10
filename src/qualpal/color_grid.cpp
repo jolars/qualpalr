41cec80db91cebb35e732c73947e7636a6f3e4ca
Johan Larsson <johanlarsson@outlook.com>
2024-06-10 16:14:00 +0200

    feat: update to qualpal lib 0.8.0

--- src/qualpal/color_grid.cpp
+++ src/qualpal/color_grid.cpp
@@ -2,27 +2,59 @@
 #include <array>
 #include <cassert>
 #include <cmath>
+#include <vector>
 
 namespace qualpal {
 
-std::vector<double>
-linspace(const std::array<double, 2>& range, const int n)
+template<int Dims>
+class Halton
 {
-  double min = range[0];
-  double max = range[1];
-
-  assert(min <= max && "min must be less than or equal to max");
-
-  std::vector<double> result;
-  result.reserve(n);
-
-  const double step = (max - min) / static_cast<double>(n - 1);
-
-  for (int i = 0; i < n; i++) {
-    result.emplace_back(min + i * step);
+public:
+  Halton()
+  {
+    static_assert(Dims <= 25, "Dimensions of more than 25 are not supported");
   }
 
-  return result;
+  std::array<double, Dims> next()
+  {
+    ++counter;
+    std::array<double, Dims> result;
+
+    for (int i = 0; i < Dims; i++) {
+      result[i] = halton(counter, primes[i]);
+    }
+
+    return result;
+  }
+
+private:
+  double halton(const int index, const int base)
+  {
+    double f = 1.0;
+    double result = 0;
+
+    int i = index;
+
+    while (i > 0) {
+      f = f / base;
+      result = result + f * (i % base);
+      i = i / base;
+    }
+
+    return result;
+  }
+
+  unsigned int counter;
+  const std::array<int, 25> primes = { 2,  3,  5,  7,  11, 13, 17, 19, 23,
+                                       29, 31, 37, 41, 43, 47, 53, 59, 61,
+                                       67, 71, 73, 79, 83, 89, 97 };
+};
+
+// Scale x from [0, 1] to [min, max]
+double
+scale_to_interval(const double x, const double min, const double max)
+{
+  return (max - min) * (x - 1) + max;
 }
 
 std::vector<HSL>
@@ -34,29 +66,19 @@ colorGrid(const std::array<double, 2>& h_lim,
   std::vector<HSL> colors;
   colors.reserve(n);
 
-  int n_i = std::round(std::cbrt(n));
+  Halton<3> halton;
 
-  assert(n_i > 0 && "n_h must be larger than zero");
+  for (int i = 0; i < n; i++) {
+    const std::array<double, 3> hsl = halton.next();
 
-  std::vector<double> h_vec = linspace(h_lim, n_i);
-  std::vector<double> s_vec = linspace(s_lim, n_i);
-  std::vector<double> l_vec = linspace(l_lim, n_i);
+    double h = scale_to_interval(hsl[0], h_lim[0], h_lim[1]);
+    double s = scale_to_interval(hsl[1], s_lim[0], s_lim[1]);
+    double l = scale_to_interval(hsl[2], l_lim[0], l_lim[1]);
 
-  for (const auto& h : h_vec) {
-    for (const auto& s : s_vec) {
-      for (const auto& l : l_vec) {
-        // We allow negative hues to wrap around the color wheel, but here we
-        // need to make sure that the hue is in the range [0, 360)
-        if (h < 0.0) {
-          colors.emplace_back(h + 360, s, l);
-        } else {
-          colors.emplace_back(h, s, l);
-        }
-      }
-    }
+    // We allow negative hues to wrap around the color wheel, but here we
+    // need to make sure that the hue is in the range [0, 360)
+    colors.emplace_back(h < 0 ? h + 360 : h, s, l);
   }
-
-  colors.shrink_to_fit();
 
   return colors;
 }
